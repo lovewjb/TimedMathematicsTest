1ab2e15d14070b3d0056a2a76430eb135206f7fa
lovewjb <18561306587@163.com>
2023-03-25 13:24:11 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,348 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.timeLabel = New System.Windows.Forms.Label()
+        Me.textLabel = New System.Windows.Forms.Label()
+        Me.plusLeftLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.plusRightLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.sum = New System.Windows.Forms.NumericUpDown()
+        Me.minusLeftLabel = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.minusRightLabel = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.difference = New System.Windows.Forms.NumericUpDown()
+        Me.timesLeftLabel = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.timesRightLabel = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.product = New System.Windows.Forms.NumericUpDown()
+        Me.dividedLeftLabel = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.dividedRightLabel = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.quotient = New System.Windows.Forms.NumericUpDown()
+        Me.startButton = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.sum, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.difference, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.product, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.quotient, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'timeLabel
+        '
+        Me.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.timeLabel.Font = New System.Drawing.Font("Monaco", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.timeLabel.Location = New System.Drawing.Point(535, 8)
+        Me.timeLabel.Name = "timeLabel"
+        Me.timeLabel.Size = New System.Drawing.Size(200, 30)
+        Me.timeLabel.TabIndex = 0
+        '
+        'textLabel
+        '
+        Me.textLabel.Font = New System.Drawing.Font("宋体", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.textLabel.Location = New System.Drawing.Point(420, 9)
+        Me.textLabel.Name = "textLabel"
+        Me.textLabel.Size = New System.Drawing.Size(109, 19)
+        Me.textLabel.TabIndex = 1
+        Me.textLabel.Text = "剩余时间:"
+        '
+        'plusLeftLabel
+        '
+        Me.plusLeftLabel.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.plusLeftLabel.Location = New System.Drawing.Point(139, 26)
+        Me.plusLeftLabel.Name = "plusLeftLabel"
+        Me.plusLeftLabel.Size = New System.Drawing.Size(50, 60)
+        Me.plusLeftLabel.TabIndex = 2
+        Me.plusLeftLabel.Text = "?"
+        Me.plusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(217, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 60)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "+"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'plusRightLabel
+        '
+        Me.plusRightLabel.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.plusRightLabel.Location = New System.Drawing.Point(296, 28)
+        Me.plusRightLabel.Name = "plusRightLabel"
+        Me.plusRightLabel.Size = New System.Drawing.Size(50, 60)
+        Me.plusRightLabel.TabIndex = 4
+        Me.plusRightLabel.Text = "?"
+        Me.plusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.Location = New System.Drawing.Point(378, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 60)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "="
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'sum
+        '
+        Me.sum.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.sum.Location = New System.Drawing.Point(453, 42)
+        Me.sum.Name = "sum"
+        Me.sum.Size = New System.Drawing.Size(100, 35)
+        Me.sum.TabIndex = 1
+        '
+        'minusLeftLabel
+        '
+        Me.minusLeftLabel.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.minusLeftLabel.Location = New System.Drawing.Point(139, 114)
+        Me.minusLeftLabel.Name = "minusLeftLabel"
+        Me.minusLeftLabel.Size = New System.Drawing.Size(50, 60)
+        Me.minusLeftLabel.TabIndex = 7
+        Me.minusLeftLabel.Text = "?"
+        Me.minusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label4.Location = New System.Drawing.Point(217, 114)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 60)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "-"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'minusRightLabel
+        '
+        Me.minusRightLabel.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.minusRightLabel.Location = New System.Drawing.Point(296, 114)
+        Me.minusRightLabel.Name = "minusRightLabel"
+        Me.minusRightLabel.Size = New System.Drawing.Size(50, 60)
+        Me.minusRightLabel.TabIndex = 9
+        Me.minusRightLabel.Text = "?"
+        Me.minusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label6.Location = New System.Drawing.Point(378, 114)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(50, 60)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "="
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'difference
+        '
+        Me.difference.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.difference.Location = New System.Drawing.Point(453, 130)
+        Me.difference.Name = "difference"
+        Me.difference.Size = New System.Drawing.Size(100, 35)
+        Me.difference.TabIndex = 2
+        '
+        'timesLeftLabel
+        '
+        Me.timesLeftLabel.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.timesLeftLabel.Location = New System.Drawing.Point(139, 204)
+        Me.timesLeftLabel.Name = "timesLeftLabel"
+        Me.timesLeftLabel.Size = New System.Drawing.Size(50, 60)
+        Me.timesLeftLabel.TabIndex = 12
+        Me.timesLeftLabel.Text = "?"
+        Me.timesLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label5.Location = New System.Drawing.Point(217, 204)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(50, 60)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "×"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'timesRightLabel
+        '
+        Me.timesRightLabel.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.timesRightLabel.Location = New System.Drawing.Point(296, 204)
+        Me.timesRightLabel.Name = "timesRightLabel"
+        Me.timesRightLabel.Size = New System.Drawing.Size(50, 60)
+        Me.timesRightLabel.TabIndex = 14
+        Me.timesRightLabel.Text = "?"
+        Me.timesRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label8.Location = New System.Drawing.Point(378, 204)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(50, 60)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "="
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'product
+        '
+        Me.product.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.product.Location = New System.Drawing.Point(453, 220)
+        Me.product.Name = "product"
+        Me.product.Size = New System.Drawing.Size(100, 35)
+        Me.product.TabIndex = 3
+        '
+        'dividedLeftLabel
+        '
+        Me.dividedLeftLabel.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.dividedLeftLabel.Location = New System.Drawing.Point(139, 293)
+        Me.dividedLeftLabel.Name = "dividedLeftLabel"
+        Me.dividedLeftLabel.Size = New System.Drawing.Size(50, 60)
+        Me.dividedLeftLabel.TabIndex = 17
+        Me.dividedLeftLabel.Text = "?"
+        Me.dividedLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label7.Location = New System.Drawing.Point(217, 293)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(50, 60)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "÷"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'dividedRightLabel
+        '
+        Me.dividedRightLabel.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.dividedRightLabel.Location = New System.Drawing.Point(296, 293)
+        Me.dividedRightLabel.Name = "dividedRightLabel"
+        Me.dividedRightLabel.Size = New System.Drawing.Size(50, 60)
+        Me.dividedRightLabel.TabIndex = 19
+        Me.dividedRightLabel.Text = "?"
+        Me.dividedRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label10
+        '
+        Me.Label10.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label10.Location = New System.Drawing.Point(378, 293)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(50, 60)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "="
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'quotient
+        '
+        Me.quotient.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.quotient.Location = New System.Drawing.Point(453, 309)
+        Me.quotient.Name = "quotient"
+        Me.quotient.Size = New System.Drawing.Size(100, 35)
+        Me.quotient.TabIndex = 4
+        '
+        'startButton
+        '
+        Me.startButton.AutoSize = True
+        Me.startButton.Font = New System.Drawing.Font("宋体", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.startButton.Location = New System.Drawing.Point(300, 374)
+        Me.startButton.Name = "startButton"
+        Me.startButton.Size = New System.Drawing.Size(161, 42)
+        Me.startButton.TabIndex = 0
+        Me.startButton.Text = "开始测验"
+        Me.startButton.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(747, 428)
+        Me.Controls.Add(Me.startButton)
+        Me.Controls.Add(Me.quotient)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.dividedRightLabel)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.dividedLeftLabel)
+        Me.Controls.Add(Me.product)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.timesRightLabel)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.timesLeftLabel)
+        Me.Controls.Add(Me.difference)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.minusRightLabel)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.minusLeftLabel)
+        Me.Controls.Add(Me.sum)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.plusRightLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.plusLeftLabel)
+        Me.Controls.Add(Me.textLabel)
+        Me.Controls.Add(Me.timeLabel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "数学测验"
+        CType(Me.sum, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.difference, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.product, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.quotient, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents timeLabel As Label
+    Friend WithEvents textLabel As Label
+    Friend WithEvents plusLeftLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents plusRightLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents sum As NumericUpDown
+    Friend WithEvents minusLeftLabel As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents minusRightLabel As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents difference As NumericUpDown
+    Friend WithEvents timesLeftLabel As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents timesRightLabel As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents product As NumericUpDown
+    Friend WithEvents dividedLeftLabel As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents dividedRightLabel As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents quotient As NumericUpDown
+    Friend WithEvents startButton As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.Threading
+Imports System.Timers
+
+Public Class Form1
+    Private randomizer As New Random
+    Private addend1 As Integer
+    Private addend2 As Integer
+    Private minuend As Integer
+    Private subtrahend As Integer
+    Private multiplicand As Integer
+    Private multiplier As Integer
+    Private dividend As Integer
+    Private divisor As Integer
+    Private timeLeft As Integer
+    Public Function CheckTheAnswer() As Boolean
+
+        If addend1 + addend2 = sum.Value AndAlso
+       minuend - subtrahend = difference.Value AndAlso
+       multiplicand * multiplier = product.Value AndAlso
+       dividend / divisor = quotient.Value Then
+
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+    Private Sub Timer1_Tick() Handles Timer1.Tick
+
+        If CheckTheAnswer() Then
+            ' If CheckTheAnswer() returns true, then the user 
+            ' got the answer right. Stop the timer  
+            ' and show a MessageBox.
+            Timer1.Stop()
+            MessageBox.Show("你把所有的题目都答对了！", "提示")
+            startButton.Enabled = True
+        ElseIf timeLeft > 0 Then
+            ' If CheckTheAnswer() returns false, keep counting
+            ' down. Decrease the time left by one second and 
+            ' display the new time left by updating the 
+            ' Time Left label.
+            timeLeft -= 1
+            timeLabel.Text = timeLeft & " seconds"
+        Else
+            ' If the user ran out of time, stop the timer, show
+            ' a MessageBox, and fill in the answers.
+            Timer1.Stop()
+            timeLabel.Text = "时间到！"
+            MessageBox.Show("对不起，你没有在时间段内完成", "提示")
+            sum.Value = addend1 + addend2
+            difference.Value = minuend - subtrahend
+            product.Value = multiplicand * multiplier
+            quotient.Value = dividend / divisor
+            startButton.Enabled = True
+        End If
+
+    End Sub
+    Public Sub StartTheQuiz()
+        addend1 = randomizer.Next(51)
+        addend2 = randomizer.Next(51)
+        plusLeftLabel.Text = addend1.ToString()
+        plusRightLabel.Text = addend2.ToString()
+        sum.Value = 0
+        ' Fill in the subtraction problem.
+        minuend = randomizer.Next(1, 101)
+        subtrahend = randomizer.Next(1, minuend)
+        minusLeftLabel.Text = minuend.ToString()
+        minusRightLabel.Text = subtrahend.ToString()
+        difference.Value = 0
+        ' Fill in the multiplication problem.
+        multiplicand = randomizer.Next(2, 11)
+        multiplier = randomizer.Next(2, 11)
+        timesLeftLabel.Text = multiplicand.ToString()
+        timesRightLabel.Text = multiplier.ToString()
+        product.Value = 0
+        ' Fill in the division problem.
+        divisor = randomizer.Next(2, 11)
+        Dim temporaryQuotient As Integer = randomizer.Next(2, 11)
+        dividend = divisor * temporaryQuotient
+        dividedLeftLabel.Text = dividend.ToString()
+        dividedRightLabel.Text = divisor.ToString()
+        quotient.Value = 0
+        timeLeft = 30
+        timeLabel.Text = "30 seconds"
+        Timer1.Start()
+    End Sub
+    Private Sub startButton_Click() Handles startButton.Click
+        StartTheQuiz()
+        startButton.Enabled = False
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles timeLabel.Click
+
+    End Sub
+
+    Private Sub fontLabel_Click(sender As Object, e As EventArgs) Handles textLabel.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles minusRightLabel.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles timesLeftLabel.Click
+
+    End Sub
+
+    Private Sub answer_Enter(sender As Object, e As EventArgs) Handles sum.Enter, quotient.Enter, product.Enter, difference.Enter
+        Dim answerBox = TryCast(sender, NumericUpDown)
+
+        If answerBox IsNot Nothing Then
+            Dim lengthOfAnswer = answerBox.Value.ToString().Length
+            answerBox.Select(0, lengthOfAnswer)
+        End If
+    End Sub
+End Class
